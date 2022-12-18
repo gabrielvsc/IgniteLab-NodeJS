8afdc61d4894b31e7ccff4a7307b997f6b5a5e57
Gabriel Vasconcelos <gabriel.cesario@ccc.ufcg.edu.br>
2022-12-18 15:07:33 -0300

    feat: count recipient notifications & tests

--- notifications-service/test/repositories/in-memory-notifications-repository.ts
+++ notifications-service/test/repositories/in-memory-notifications-repository.ts
@@ -2,7 +2,6 @@ import { Notification } from "@application/entities/notification";
 import { NotificationsRepository } from "@application/repositories/notifications-repository";
 
 export class InMemoryNotificationsRepository implements NotificationsRepository {
-    
     public notifications: Notification[] = [];
     
     async findById(notificationId: string): Promise<Notification | null> {
@@ -13,6 +12,10 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
         }
 
         return notification;
+    }
+
+    async countManyByRecipientId(recipientId: string): Promise<number> {
+        return this.notifications.filter(notification => notification.recipientId === recipientId).length;
     }
 
     async create(notification: Notification) {
